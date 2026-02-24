e45ce210ce2e8fe003e14e644188afd5270aa8d0
KAMLSH KUMAR <143285346+kamlesh-kr23@users.noreply.github.com>
2026-02-24 16:09:52 +0530

    Switch database backend to SQLite
    Changed database configuration from dj_database_url to SQLite.

--- crop_site/settings.py
+++ crop_site/settings.py
@@ -78,9 +78,11 @@ TEMPLATES = [
 import dj_database_url
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL')
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+
 }
 
 
